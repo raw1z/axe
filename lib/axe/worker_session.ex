faf800512de03f74915d1eaccc12b361dbdb21e2
Rawane Zossou <dev@raw1z.fr>
2014-09-30 01:04:51 +0200

    move the SessionData module to a separate file

--- lib/axe/worker_session.ex
+++ lib/axe/worker_session.ex
@@ -2,18 +2,7 @@ defmodule Axe.WorkerSession do
   use Axe.GenFSM
   require Logger
 
-  defmodule SessionData do
-    defstruct ref: nil, url: nil, requester: nil, status_code: nil, resp_headers: nil, info: nil, data: "", req_headers: nil, req_method: nil, req_body: nil
-
-    def location(session) do
-      get_location(session.resp_headers)
-    end
-
-    defp get_location([]), do: nil
-    defp get_location([{"Location", location}|_]), do: location
-    defp get_location([{"location", location}|_]), do: location
-    defp get_location([_|tail]), do: get_location(tail)
-  end
+  alias Axe.SessionData
 
   require Record
   Record.defrecord :hackney_url, Record.extract(:hackney_url, from_lib: "hackney/include/hackney_lib.hrl")
